18de360df3ff143822c554fd534b40f7f10963b8
Huarzone <101872541+huarzone@users.noreply.github.com>
2022-04-03 10:41:40 +0800

    Delete randomPic.ts

--- src/pages/api/randomPic.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import {NextApiRequest, NextApiResponse} from "next";
-
-
-const randomPic = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.redirect(307, `https://api.mtyqx.cn/api/random.php`)
-}
-export default random
