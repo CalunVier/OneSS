fb16bcfd9e297110814afab384ee8f37c131dfd8
Huarzone <101872541+huarzone@users.noreply.github.com>
2022-04-03 09:50:52 +0800

    Update userList.ts

--- setting/userList.ts
+++ setting/userList.ts
@@ -1,7 +1,7 @@
 const userList: userListType = {
     users: ["share@m.huarzone.com", "nf@m.huarzone.com", "ct@m.huarzone.com", "mv@m.huarzone.com"],
     nickname: {
-        "share@m.huarzone.com": '✨ 我的对象存储主站',
+        "share@m.huarzone.com": '✨ 我的对象存储主站 🐳',
         "nf@m.huarzone.com": '🔥 影视对象存储服务-NF',
         "ct@m.huarzone.com": '🐻 影视对象存储服务-CT',
         "mv@m.huarzone.com": '🍭 影视对象存储服务-MV',
