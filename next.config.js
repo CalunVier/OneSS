da37aeb311be301a06f00a521d8b011bee9f9156
Huarzone <101872541+huarzone@users.noreply.github.com>
2022-04-03 12:11:44 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['oness.huarzone.com'],
+        domains: ['mystatic.dzaaaaaa.com', 'image.huarzone.com', 'random.huarzone.com'],
     },
     reactStrictMode: true,
 }
